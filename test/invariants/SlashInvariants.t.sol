cea931596fa42194ef802a92ca6b2bf2975ee1e6
chad-js <chadflemies@gmail.com>
2024-02-16 14:42:23 -0500

    Add more slash invariants

--- test/invariants/SlashInvariants.t.sol
+++ test/invariants/SlashInvariants.t.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.22;
 
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {Ownable} from "cozy-safety-module-shared/lib/Ownable.sol";
 import {IERC20} from "cozy-safety-module-shared/interfaces/IERC20.sol";
 import {ICommonErrors} from "cozy-safety-module-shared/interfaces/ICommonErrors.sol";
 import {MathConstants} from "cozy-safety-module-shared/lib/MathConstants.sol";
@@ -11,6 +12,7 @@ import {SafetyModuleState} from "../../src/lib/SafetyModuleStates.sol";
 import {Slash} from "../../src/lib/structs/Slash.sol";
 import {Trigger} from "../../src/lib/structs/Trigger.sol";
 import {TriggerState} from "../../src/lib/SafetyModuleStates.sol";
+import {ISlashHandlerErrors} from "../../src/interfaces/ISlashHandlerErrors.sol";
 import {ITrigger} from "../../src/interfaces/ITrigger.sol";
 import {IStateChangerErrors} from "../../src/interfaces/IStateChangerErrors.sol";
 import {MockTrigger} from "../utils/MockTrigger.sol";
@@ -26,6 +28,11 @@ abstract contract StateTransitionInvariantsWithStateTransitions is InvariantTest
   mapping(IERC20 => uint256) expectedAssetSlashAmounts;
   mapping(IERC20 => uint256) assetPoolsBeforeSlash;
 
+  struct InternalReservePoolBalances {
+    uint256 depositAmount;
+    uint256 pendingWithdrawalsAmount;
+  }
+
   function invariant_slashAccountingUpdates() public syncCurrentTimestamp(safetyModuleHandler) {
     ITrigger selectedTrigger_ = safetyModuleHandler.pickValidTrigger(_randomUint256());
     // If there are no valid triggers, we skip this invariant.
@@ -37,14 +44,16 @@ abstract contract StateTransitionInvariantsWithStateTransitions is InvariantTest
     // Trigger the safety module putting it in triggered or paused state.
     safetyModule.trigger(selectedTrigger_);
 
+    // If the safety module is not in the triggered state, we skip these invariants.
     if (safetyModule.safetyModuleState() != SafetyModuleState.TRIGGERED) return;
 
     Slash[] memory slashes_ = new Slash[](numReservePools);
-    uint256[] memory reservePoolDepositAmountsBeforeSlash_ = new uint256[](numReservePools);
+    InternalReservePoolBalances[] memory reservePoolBeforeSlash_ = new InternalReservePoolBalances[](numReservePools);
     for (uint8 i = 0; i < numReservePools; i++) {
       ReservePool memory reservePool_ = safetyModule.reservePools(i);
       slashes_[i] = Slash({reservePoolId: i, amount: _pickSlashAmount(i)});
-      reservePoolDepositAmountsBeforeSlash_[i] = reservePool_.depositAmount;
+      reservePoolBeforeSlash_[i].depositAmount = reservePool_.depositAmount;
+      reservePoolBeforeSlash_[i].pendingWithdrawalsAmount = reservePool_.pendingWithdrawalsAmount;
       expectedAssetSlashAmounts[reservePool_.asset] += slashes_[i].amount;
     }
 
@@ -60,15 +69,53 @@ abstract contract StateTransitionInvariantsWithStateTransitions is InvariantTest
       ReservePool memory reservePool_ = safetyModule.reservePools(i);
 
       require(
-        reservePoolDepositAmountsBeforeSlash_[i] - reservePool_.depositAmount == slashes_[i].amount,
+        reservePoolBeforeSlash_[i].depositAmount - reservePool_.depositAmount == slashes_[i].amount,
         string.concat(
           "Invariant failed: Reserve pool deposit amount slashed does not equal the amount specified in the slash.",
-          " reservePoolDepositAmountsBeforeSlash_[i]: ",
-          Strings.toString(reservePoolDepositAmountsBeforeSlash_[i]),
+          " reservePoolBeforeSlash_[i].depositAmount: ",
+          Strings.toString(reservePoolBeforeSlash_[i].depositAmount),
           ", reservePool_.depositAmount: ",
           Strings.toString(reservePool_.depositAmount),
           ", slashes_[i].amount: ",
           Strings.toString(slashes_[i].amount)
+        )
+      );
+
+      require(
+        reservePoolBeforeSlash_[i].pendingWithdrawalsAmount - reservePool_.pendingWithdrawalsAmount
+          <= slashes_[i].amount
+        // The pending withdrawals amount slashed is rounded up.
+        || (reservePoolBeforeSlash_[i].pendingWithdrawalsAmount - reservePool_.pendingWithdrawalsAmount)
+          - slashes_[i].amount == 1,
+        string.concat(
+          "Invariant failed: Reserve pool pending withdrawals slashed is not lte the amount specified in the slash.",
+          " reservePoolBeforeSlash_[i].pendingWithdrawalsAmount: ",
+          Strings.toString(reservePoolBeforeSlash_[i].pendingWithdrawalsAmount),
+          ", reservePool_.pendingWithdrawalsAmount: ",
+          Strings.toString(reservePool_.pendingWithdrawalsAmount),
+          ", slashes_[i].amount: ",
+          Strings.toString(slashes_[i].amount),
+          ", reservePool.depositAmount: ",
+          Strings.toString(reservePool_.depositAmount)
+        )
+      );
+
+      uint256 pendingRedemptionsPercentageChange_ = reservePoolBeforeSlash_[i].pendingWithdrawalsAmount == 0
+        ? 0
+        : reservePool_.pendingWithdrawalsAmount.mulDivDown(
+          MathConstants.ZOC, reservePoolBeforeSlash_[i].pendingWithdrawalsAmount
+        );
+      uint256 depositAmountPercentageChange_ = reservePoolBeforeSlash_[i].depositAmount == 0
+        ? 0
+        : reservePool_.depositAmount.mulDivDown(MathConstants.ZOC, reservePoolBeforeSlash_[i].depositAmount);
+      require(
+        pendingRedemptionsPercentageChange_ <= depositAmountPercentageChange_,
+        string.concat(
+          "Invariant failed: The percentage change in pending redemptions must be less than or equal to the percentage change in deposit amount.",
+          " pendingRedemptionsPercentageChange_: ",
+          Strings.toString(pendingRedemptionsPercentageChange_),
+          ", depositAmountPercentageChange_: ",
+          Strings.toString(depositAmountPercentageChange_)
         )
       );
     }
@@ -89,10 +136,92 @@ abstract contract StateTransitionInvariantsWithStateTransitions is InvariantTest
     }
   }
 
-  function _pickRandomTriggerAndTriggerData() internal view returns (ITrigger, Trigger memory) {
-    ITrigger[] memory triggers_ = safetyModuleHandler.getTriggers();
-    ITrigger selectedTrigger_ = triggers_[_randomUint256() % triggers_.length];
-    return (selectedTrigger_, safetyModule.triggerData(selectedTrigger_));
+  function invariant_cannotSlashReservePoolMoreThanOnceInSingleTx() public syncCurrentTimestamp(safetyModuleHandler) {
+    if (numReservePools == 1) return;
+    ITrigger selectedTrigger_ = safetyModuleHandler.pickValidTrigger(_randomUint256());
+    // If there are no valid triggers, we skip this invariant.
+    if (selectedTrigger_ == ITrigger(safetyModuleHandler.DEFAULT_ADDRESS())) return;
+    Trigger memory selectedTriggerData_ = safetyModule.triggerData(selectedTrigger_);
+    // Trigger the trigger.
+    MockTrigger(address(selectedTrigger_)).mockState(TriggerState.TRIGGERED);
+
+    // Trigger the safety module putting it in triggered or paused state.
+    safetyModule.trigger(selectedTrigger_);
+
+    // If the safety module is not in the triggered state, we skip these invariants.
+    if (safetyModule.safetyModuleState() != SafetyModuleState.TRIGGERED) return;
+
+    // Randomize number of reservePools to slash.
+    Slash[] memory slashes_ = new Slash[](bound(_randomUint256() % numReservePools, 2, numReservePools));
+    for (uint8 i = 0; i < slashes_.length; i++) {
+      slashes_[i] = Slash({reservePoolId: i, amount: _pickSlashAmount(i)});
+    }
+
+    // Randomize which reservePool slash is duped.
+    uint256 dupedA_ = _randomUint256() % slashes_.length;
+    bool useBefore_ = (dupedA_ != 0 && _randomUint256() % 2 == 0) || dupedA_ == slashes_.length - 1;
+    uint256 dupedB_ = useBefore_
+      ? bound(_randomUint256() % slashes_.length, 0, dupedA_ - 1)
+      : bound(_randomUint256() % slashes_.length, dupedA_ + 1, slashes_.length - 1);
+    slashes_[dupedB_] = slashes_[dupedA_];
+
+    // Attempt to slash the safety module.
+    vm.prank(selectedTriggerData_.payoutHandler);
+    vm.expectRevert(
+      abi.encodeWithSelector(ISlashHandlerErrors.AlreadySlashed.selector, slashes_[dupedA_].reservePoolId)
+    );
+    safetyModule.slash(slashes_, _randomAddress());
+  }
+
+  function invariant_cannotSlashIfSafetyModuleNotTriggered() public syncCurrentTimestamp(safetyModuleHandler) {
+    if (safetyModule.safetyModuleState() == SafetyModuleState.TRIGGERED) return;
+
+    for (uint256 i = 0; i < triggers.length; i++) {
+      ITrigger trigger_ = triggers[i];
+      Trigger memory triggerData_ = safetyModule.triggerData(trigger_);
+
+      // Attempt to slash the safety module.
+      if (safetyModule.payoutHandlerNumPendingSlashes(triggerData_.payoutHandler) == 0) {
+        vm.expectRevert(Ownable.Unauthorized.selector);
+      } else {
+        vm.expectRevert(ICommonErrors.InvalidState.selector);
+      }
+      vm.prank(triggerData_.payoutHandler);
+      safetyModule.slash(new Slash[](0), _randomAddress());
+    }
+  }
+
+  function invariant_cannotSlashMoreThanMaxSlashPercentage() public syncCurrentTimestamp(safetyModuleHandler) {
+    ITrigger selectedTrigger_ = safetyModuleHandler.pickValidTrigger(_randomUint256());
+    // If there are no valid triggers, we skip this invariant.
+    if (selectedTrigger_ == ITrigger(safetyModuleHandler.DEFAULT_ADDRESS())) return;
+    Trigger memory selectedTriggerData_ = safetyModule.triggerData(selectedTrigger_);
+    // Trigger the trigger.
+    MockTrigger(address(selectedTrigger_)).mockState(TriggerState.TRIGGERED);
+
+    // Trigger the safety module putting it in triggered or paused state.
+    safetyModule.trigger(selectedTrigger_);
+
+    // If the safety module is not in the triggered state, we skip these invariants.
+    if (safetyModule.safetyModuleState() != SafetyModuleState.TRIGGERED) return;
+
+    Slash[] memory slashes_ = new Slash[](1);
+    uint8 reservePoolId_ = safetyModuleHandler.pickValidReservePoolId(_randomUint256());
+    ReservePool memory reservePool_ = safetyModule.reservePools(reservePoolId_);
+
+    if (reservePool_.depositAmount == 0) return;
+
+    uint256 maxSlashAmount_ = safetyModule.getMaxSlashableReservePoolAmount(reservePoolId_);
+    uint256 amountToSlash_ = bound(_randomUint256(), maxSlashAmount_ + 1, type(uint128).max);
+    uint256 slashPercentage_ = amountToSlash_.mulDivUp(MathConstants.ZOC, reservePool_.depositAmount);
+    slashes_[0] = Slash({reservePoolId: reservePoolId_, amount: amountToSlash_});
+
+    // Attempt to slash the safety module.
+    vm.prank(selectedTriggerData_.payoutHandler);
+    vm.expectRevert(
+      abi.encodeWithSelector(ISlashHandlerErrors.ExceedsMaxSlashPercentage.selector, reservePoolId_, slashPercentage_)
+    );
+    safetyModule.slash(slashes_, _randomAddress());
   }
 
   function _pickSlashAmount(uint8 reservePoolId_) internal view returns (uint256) {
