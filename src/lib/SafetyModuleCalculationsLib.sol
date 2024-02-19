f1d153f0664dd69d51129ad2328d85997456cf6a
Raghav Bansal <raghavb1234@gmail.com>
2024-02-19 11:05:26 -0500

    Add comment about pool amount floor

--- src/lib/SafetyModuleCalculationsLib.sol
+++ src/lib/SafetyModuleCalculationsLib.sol
@@ -24,7 +24,7 @@ library SafetyModuleCalculationsLib {
   }
 
   /// @notice The `assetAmount_` that the safety module would exchange for `receiptTokenAmount_` of the receipt
-  /// token.
+  /// token. Note that we do not floor the pool amount here, as the pool amount never shows up in the denominator.
   /// @dev See the ERC-4626 spec for more info.
   function convertToAssetAmount(uint256 receiptTokenAmount_, uint256 receiptTokenSupply_, uint256 poolAmount_)
     internal
