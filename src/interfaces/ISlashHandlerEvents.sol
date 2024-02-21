83070410e6e92142779ec930b138aac55554e596
chad-js <chadflemies@gmail.com>
2024-02-21 11:47:37 -0500

    Fix typos

--- src/interfaces/ISlashHandlerEvents.sol
+++ src/interfaces/ISlashHandlerEvents.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 interface ISlashHandlerEvents {
-  ///dev Emitted when a reserve pool is slashed.
+  /// @dev Emitted when a reserve pool is slashed.
   event Slashed(
     address indexed payoutHandler_, address indexed receiver_, uint256 indexed reservePoolId_, uint256 assetAmount_
   );
--- src/interfaces/IStateChangerEvents.sol
+++ src/interfaces/IStateChangerEvents.sol
@@ -5,9 +5,9 @@ import {SafetyModuleState} from "../lib/SafetyModuleStates.sol";
 import {ITrigger} from "../interfaces/ITrigger.sol";
 
 interface IStateChangerEvents {
-  /// @notice Emitted when the SafetyModule changes state.
+  /// @dev Emitted when the SafetyModule changes state.
   event SafetyModuleStateUpdated(SafetyModuleState indexed updatedTo_);
 
-  // Emitted when the SafetyModule is triggered.
+  /// @dev Emitted when the SafetyModule is triggered.
   event Triggered(ITrigger indexed trigger);
 }
